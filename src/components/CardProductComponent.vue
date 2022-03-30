363843acdb4e4b599c8286958c986ac954df4083
Wandy Hanyudha <hanyudha@uinjkt.ac.id>
2022-03-30 15:10:48 +0700

    fix card product.gambar

--- src/components/CardProductComponent.vue
+++ src/components/CardProductComponent.vue
@@ -1,22 +1,20 @@
 <template>
-    <div class="card">
-    <img :src="'/assets/images/'+product.gambar" class="card-img-top" />
-        <div class="card-body">
-            <h5 class="card-title">{{product.nama}}</h5>
-            <p class="card-text">
-                Some quick Example
-            </p>
-            <a href="#" class="btn btn-primary">Go Somewhere</a>
-        </div>
+  <div class="card">
+    <!--<img v-bind:src=" '../assets/images/' + product.gambar " class="card-img-top" />-->
+    <img :src="require('@/assets/images/' +product.gambar)" />
+    <div class="card-body">
+      <h5 class="card-title">{{ product.nama }}</h5>
+      <p class="card-text">Some quick Example</p>
+      <a href="#" class="btn btn-primary">Go Somewhere</a>
     </div>
+  </div>
 </template>
 
 <script>
-export default{
-    name: 'CardProductComponent',
-    props: ['product']
-}
+export default {
+  name: "CardProductComponent",
+  props: ["product"],
+};
 </script>
 
-<style>
-</style>
+<style></style>
